131707cdc2dd313ffa350ddabed48d8db8dc84f8
kasundraJeet <kasundrajeet29@gmail.com>
2025-01-18 20:27:45 +0530

    server side logic

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <main>
-        yayya   
+        yayya
         <slot />
     </main>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
+<script setup>
+import { useFetch } from '#app';
+
+const { data, error, refresh } = useFetch('https://api.themoviedb.org/3/person/popular?language=en-US&page=1', {
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${useRuntimeConfig().TMDB_TOKEN}`,
+  },
+});
+
+console.log(data);
+</script>
+
 <template>
     home page
 </template>
